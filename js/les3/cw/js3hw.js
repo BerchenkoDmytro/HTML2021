6500c30cf0046e08f3f1ff9cfd10419576fad853
BerchenkoDmytro <bera1795@rambler.ru>
2021-11-18 14:49:12 +0200

    js les3 hw

--- js/les3/cw/js3hw.js
+++ js/les3/cw/js3hw.js
@@ -150,8 +150,9 @@
 
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
 
-for (let i = 0; i < 100; i++) {
-   if (i % 2 === 1 ) {
-       console.log(i);
-   }
-}
+// for (let i = 0; i < 100; i++) {
+//    if (i % 2 === 1 ) {
+//        console.log(i);
+//    }
+// }
+
